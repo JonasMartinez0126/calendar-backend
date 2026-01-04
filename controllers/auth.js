47b178f7c74e90f0d398c4f6001055c914cfca20
Jonas Martinez <jonasmartinez0126@gmail.com>
2026-01-04 15:09:47 -0300

    Se inicio el proyecto de calendar del lado del backend, se realizaron las rutas, controladores para el lado de la autenticacion de usuarios. Ademas se realizo la coneccion de la base de datos de MONGO Atlas

---
+++ controllers/auth.js
@@ -0,0 +1,104 @@
+const { response } = require("express");
+const bcrypt = require("bcryptjs");
+const Usuario = require("../models/Usuario");
+const { generarJWT } = require("../helpers/jwt");
+
+const crearUsuario = async (req, res = response) => {
+  const { email, password } = req.body;
+
+  try {
+    let usuario = await Usuario.findOne({ email });
+
+    if (usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "En usuario existe con ese correo",
+      });
+    }
+
+    usuario = new Usuario(req.body);
+
+    // encriptar contraseña
+    const salt = bcrypt.genSaltSync();
+    usuario.password = bcrypt.hashSync(password, salt);
+
+    // guardar usuario
+    await usuario.save();
+
+    // generar JWT
+    const token = await generarJWT(usuario.id, usuario.name);
+
+    res.status(201).json({
+      ok: true,
+      uid: usuario.id,
+      name: usuario.name,
+      token,
+      msg: "Registro exitoso!",
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: "Por favor hable con el administrador",
+    });
+  }
+};
+
+const loginUsuario = async (req, res = response) => {
+  const { email, password } = req.body;
+
+  try {
+    let usuario = await Usuario.findOne({ email });
+
+    if (!usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Usuario o contraseña no son correctos",
+      });
+    }
+
+    // validar password
+    const validPassword = bcrypt.compareSync(password, usuario.password);
+
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Usuario o contraseña no son correctos",
+      });
+    }
+
+    // generar JWT
+    const token = await generarJWT(usuario.id, usuario.name);
+
+    res.status(201).json({
+      ok: true,
+      uid: usuario.id,
+      name: usuario.name,
+      token,
+      msg: "Login exitoso!",
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: "Por favor hable con el administrador",
+    });
+  }
+};
+
+const revalidarToken = async (req, res = response) => {
+  const uid = req.uid;
+  const name = req.name;
+
+  // generar un nuevo JWT
+  const token = await generarJWT(uid, name);
+
+  res.json({
+    ok: true,
+    token,
+  });
+};
+
+module.exports = {
+  crearUsuario,
+  loginUsuario,
+  revalidarToken,
+};
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { dbConnection } = require("./database/config");
+const cors = require("cors");
+require("dotenv").config();
+
+// crear el servidor de express
+const app = express();
+
+// base de datos
+dbConnection();
+
+// cors
+app.use(cors());
+
+// Directorio publico
+app.use(express.static("public"));
+
+// lectura y parseo del body
+app.use(express.json());
+
+// Rutas
+// autenticacion
+app.use("/api/auth", require("./routes/auth"));
+// CRUD de eventos
+
+// escuchar peticiones
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo ${process.env.PORT}`);
+});
