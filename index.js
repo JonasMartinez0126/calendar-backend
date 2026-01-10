2ae04aa3239f930b382769c90b6d6c591a10accc
Jonas Martinez <jonasmartinez0126@gmail.com>
2026-01-10 14:51:12 -0300

    Se agrego frontend en el public y se modifico el index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const { dbConnection } = require("./database/config");
 const cors = require("cors");
@@ -23,6 +24,10 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 // CRUD de eventos
 app.use("/api/events", require("./routes/events"));
+
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 // escuchar peticiones
 app.listen(process.env.PORT, () => {
